c62b6546c2702fc9dcaeca0155a9419eafb54309
Lucas Neves <lucas.neves@codeblox.com.br>
2021-01-04 20:10:52 -0300

    Parsing request bodies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs  = require('fs');
+const { parse } = require('path');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -22,7 +23,16 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'Dummy');
+    const body = []
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    })
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();

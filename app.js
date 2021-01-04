9fd13d8c862c824e374268c13bdc3c1100976086
Lucas Neves <lucas.neves@codeblox.com.br>
2021-01-04 19:55:00 -0300

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-const http = require('http')
+const http = require('http');
+const fs  = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    
+    res.write(`
+      <html>
+        <head>
+          <title>Enter Message</title>
+          <body>
+            <form action="/message" method="POST">
+              <input type="text" name="message" />
+              <button type="submit">Send</button>
+            </form>
+          </body>
+        </head>
+      </html>
+    `)
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'Dummy');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write(`
+      <html>
+        <head>
+          <title>Mi First page with Node</title>
+          <body>
+            <h1>Hello from node server</h1>
+          </body>
+        </head>
+      </html>
+    `)
+    res.end();
 });
 
 server.listen(3000)
